8a37cceff68002dbfd8161a2f8fb6f8857da25dc
Umair Khaliq <umairkhaliq.ai@gmail.com>
2026-04-01 07:23:27 +0100

    Day 1 completed: variables, types, print, concatenation, Euclidean distance

---
+++ day1_euclidean.py
@@ -0,0 +1,3 @@
+import math 
+distance = math.sqrt((10 - 2)**2 + (8 - 3)**2)
+print(distance)
---
+++ day1_test.py
@@ -0,0 +1,8 @@
+age = 25
+name = "Umair Khaliq"
+print(age, name)
+print(type(25))
+print(type(3.14))
+print(type("Umair"))
+print(type(True))
+print(type([1, 2, 3]))
