2ddfdb9adbe05aeb73f9278ba8c50bdee7d44f32
Umair Khaliq <umairkhaliq.ai@gmail.com>
2026-04-13 11:53:21 +0100

    Reorganize files into Day 1 and Day 2 folders

---
+++ 03_Day_Operators/assignment_example.py
@@ -0,0 +1,2 @@
+print(" ==== Assignment Operator ==== ")
+print(" Assignment Operator (=) is used to assign a value to a variable. It takes the value on the right side and stores it in the variable on the left side.")
